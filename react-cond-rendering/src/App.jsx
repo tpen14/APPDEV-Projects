707b85a24a2951763540365eb8b7a0f8fc1b464b
Stephen Robles <20161521@s.ubaguio.edu>
2024-10-07 15:14:39 +0800

    Update App.jsx

--- react-cond-rendering/src/App.jsx
+++ react-cond-rendering/src/App.jsx
@@ -13,16 +13,16 @@ function App() {
     {id: 1, name: "cake", color: "red"},
     {id: 2, name: "ice creat", color: "green"},
     {id: 3, name: "pie", color: "blue"}
-
-
   ]
-  
+  fruits.sort((a , b) => b.name.localeCompare(a.name))
+  const redfruit = fruits.filter (fruit => fruit.color === "red")
+
   return (
     
     <>
     <Header/>
     {fruits.length > 0 ? <ListOfFruits items = {fruits} category = "My Favorite Fruits"/>:(<p>I do not like fruits!</p>)}  
-    {fruits.length > 0 && <ListOfFruits items = {fruits} category = "My Favorite Fruits"/>}
+    {redfruit.length > 0 && <ListOfFruits items = {redfruit} category = "My Favorite Fruits"/>}
     {desserts.length > 0 && <ListOfFruits items={desserts} category ="My Favorite Desserts"/>}
     <Footer/>    
     </>
